bec9d86a70afbc67f6ab19ff88dffefa44c771f2
wuw6248 <87937525+wuw6248@users.noreply.github.com>
2022-05-22 01:16:02 +0700

    Add files via upload

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export {HttpClient} from './HttpClient';
+export {HttpResponse} from './HttpResponse';
+export {HttpHeader} from './HttpHeader';
+export {HttpRequest} from './HttpRequest';
+export {HttpRequestMethod} from './HttpRequestMethod';
