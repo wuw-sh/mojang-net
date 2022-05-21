260c03a8e4720f5ce46423116a1f5f14905d7ff4
wuw6248 <87937525+wuw6248@users.noreply.github.com>
2022-05-22 01:27:38 +0700

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export {HttpClient} from './HttpClient';
-export {HttpResponse} from './HttpResponse';
-export {HttpHeader} from './HttpHeader';
-export {HttpRequest} from './HttpRequest';
-export {HttpRequestMethod} from './HttpRequestMethod';
